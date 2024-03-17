80cbf417fb054e22dffa087593b96390ae336034
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2024-03-17 19:55:49 +0530

    Update ContactForm.jsx

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -85,9 +85,10 @@ export default function ContactForm() {
 
       <div className="bg-slate-100 flex flex-col">
         {error &&
-          error.map((e) => (
-            <div
-              className={`${success ? "text-green-800" : "text-red-600"} px-5 py-2`}
+          error.map((e, index) => (
+            <div key={index}
+              className={`${success ? "text-green-800" : "text-red-600"
+              } px-5 py-2`}
             >
               {e}
             </div>
